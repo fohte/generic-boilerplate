067705b9ea5ecd3c9fd0745e6fca065459ec195d
Fohte / Hayato Kawai <fohte.hk@gmail.com>
2026-07-26 20:17:56 +0900

    feat(template)!: add neverthrow-based error handling scaffolding (#527)

--- generated/node/eslint.config.js
+++ generated/node/eslint.config.js
@@ -2,7 +2,10 @@ import { config } from '@fohte/eslint-config'
 import storybook from 'eslint-plugin-storybook'
 
 export default config(
-  { typescript: { typeChecked: true } },
+  {
+    typescript: { typeChecked: true },
+    errorHandling: {},
+  },
   ...storybook.configs['flat/recommended'],
   {
     rules: {
