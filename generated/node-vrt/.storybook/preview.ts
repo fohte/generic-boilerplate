2ed9f9d627154a20d69c1a69251b7164061643c7
Fohte / Hayato Kawai <fohte.hk@gmail.com>
2026-09-24 15:32:43 +0900

    feat: support Storybook and VRT generation in root Node.js packages (#706)

--- generated/node-vrt/.storybook/preview.ts
+++ generated/node-vrt/.storybook/preview.ts
@@ -1,16 +1,11 @@
-import '#index.css'
-
 import {
   afterEach,
   beforeEach,
   configureUnhandledApiRequestCheck,
   parameters,
-  reportUnhandledApiRequest,
 } from '@fohte/storybook-addon/preview'
 import { withThemeByClassName } from '@storybook/addon-themes'
 import type { Preview } from '@storybook/react-vite'
-import { setupWorker } from 'msw/browser'
-import { mswLoader } from 'msw-storybook-addon/csf3'
 
 configureUnhandledApiRequestCheck({ pathPrefixes: ['/api/'] })
 
@@ -29,19 +24,6 @@ const preview: Preview = {
         dark: 'dark',
       },
       defaultTheme: 'light',
-    }),
-  ],
-  loaders: [
-    mswLoader(async () => {
-      const worker = setupWorker()
-      await worker.start({
-        onUnhandledRequest: (request, print) => {
-          if (reportUnhandledApiRequest(request.url)) {
-            print.error()
-          }
-        },
-      })
-      return worker
     }),
   ],
 }
