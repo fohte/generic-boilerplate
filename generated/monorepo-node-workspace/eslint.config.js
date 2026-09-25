a17bebf561de375edb14bd683a33a0f55b1a269e
Fohte / Hayato Kawai <fohte.hk@gmail.com>
2026-09-25 22:02:33 +0900

    feat(template): enforce `name` property on Storybook stories (#713)

--- generated/monorepo-node-workspace/eslint.config.js
+++ generated/monorepo-node-workspace/eslint.config.js
@@ -29,4 +29,8 @@ export default config(
     ],
     rules: { 'no-restricted-imports': 'off' },
   },
+  {
+    files: ['**/*.stories.tsx'],
+    rules: { 'fohte/require-story-name': 'error' },
+  },
 )
